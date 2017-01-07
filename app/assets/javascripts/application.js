666219c3078d4fd039d496762eb04ec7d91802d1
Karla <karlarobinson5@gmail.com>
2017-01-07 17:59:29 -0600

    tokenize successful, failes to charge

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,37 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var conektaSuccessResponseHandler = function(token) {
+  var $form = $("#card-form");
+
+  /* Inserta el token_id en la forma para que se envíe al servidor */
+  $form.append($("<input type='hidden' name='conektaTokenId'>").val(token.id));
+ 
+  /* and submit */
+  $form.get(0).submit();
+};
+
+var conektaErrorResponseHandler = function(response) {
+  var $form = $("#card-form");
+  
+  /* Muestra los errores en la forma */
+  $form.find(".card-errors").text(response.message_to_purchaser);
+  $form.find("button").prop("disabled", false);
+};
+
+$(function () {
+  $("#card-form").submit(function(event) {
+    var $form = $(this);
+
+    // Previene hacer submit más de una vez
+    $form.find("button").prop("disabled", true);
+    Conekta.token.create($form, conektaSuccessResponseHandler, conektaErrorResponseHandler);
+   //Conekta.Token.create($form, conektaSuccessResponseHandler, conektaErrorResponseHandler); //v5+
+   
+    // Previene que la información de la forma sea enviada al servidor
+    return false;
+  });
+});
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get  '/card_form',          to: 'static_pages#card_form'
+  post '/charge',          to: 'static_pages#charge'
 end
