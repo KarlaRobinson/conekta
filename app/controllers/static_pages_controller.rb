4e95e06b2cc55f0e3d0252a495de47f3e877470b
Karla <karlarobinson5@gmail.com>
2017-01-08 18:20:26 -0600

    allow ondemand and subscriptions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,6 +9,7 @@ class StaticPagesController < ApplicationController
 		    "description"=> "Pizza Delivery",
 		    "reference_id"=> "orden_de_id_interno",
 		    "card"=> params[:conektaTokenId],  # Ej. "tok_a4Ff0dD2xYZZq82d9" tok_test_visa_4242
+		    #"card"=>"tok_test_visa_4242",
 		    "details"=> {
 		      "name"=> "Arnulfo Quimare",
 		      "phone"=> "403-342-0642",
@@ -172,7 +173,6 @@ class StaticPagesController < ApplicationController
 		rescue Conekta::Error => e
 		  puts e.message_to_purchaser 
 		  #un error ocurrió que no sucede en el flujo normal de cobros como por ejemplo un auth_key incorrecto
-
 		end
 	end
 
@@ -248,5 +248,102 @@ class StaticPagesController < ApplicationController
 		  #un error ocurrió que no sucede en el flujo normal de cobros como por ejemplo un auth_key incorrecto
 
 		end
-end
+	end
+
+	def ondemand
+		begin
+		  @customer = Conekta::Customer.create({
+		    name: "Lews Therin",
+		    email: "lews.therin@gmail.com",
+		    phone: "55-5555-5555",
+		    cards: ["tok_test_visa_4242"]
+		    #cards: [params[:conektaTokenId]]  #["tok_a4Ff0dD2xYZZq82d9"]
+		  })
+		rescue Conekta::ParameterValidationError => e
+		  puts e.message_to_purchaser 
+		  #alguno de los parámetros fueron inválidos
+		end
+	
+		begin
+		  @charge = Conekta::Charge.create({
+		    "amount"=> 51000,
+		    "currency"=> "MXN",
+		    "description"=> "Pizza Delivery",
+		    "reference_id"=> "orden_de_id_interno",
+		    "card"=> @customer.id,
+		    "details"=> {
+		      "name"=> "Arnulfo Quimare",
+		      "phone"=> "403-342-0642",
+		      "email"=> "logan@x-men.org",
+		      "customer"=> {
+		        "logged_in"=> true,
+		        "successful_purchases"=> 14,
+		        "created_at"=> 1379784950,
+		        "updated_at"=> 1379784950,
+		        "offline_payments"=> 4,
+		        "score"=> 9
+		      },
+		      "line_items"=> [{
+		        "name"=> "Box of Cohiba S1s",
+		        "description"=> "Imported From Mex.",
+		        "unit_price"=> 20000,
+		        "quantity"=> 1,
+		        "sku"=> "cohb_s1",
+		        "category"=> "food"
+		      }],
+		      "billing_address"=> {
+		        "street1"=>"77 Mystery Lane",
+		        "street2"=> "Suite 124",
+		        "city"=> "Darlington",
+		        "state"=>"NJ",
+		        "zip"=> "10192",
+		        "country"=> "Mexico",
+		        "tax_id"=> "xmn671212drx",
+		        "company_name"=>"X-Men Inc.",
+		        "phone"=> "77-777-7777",
+		        "email"=> "purshasing@x-men.org"
+		      }
+		    }
+		  })
+		rescue Conekta::ParameterValidationError => e
+		  puts e.message_to_purchaser 
+		  #alguno de los parámetros fueron inválidos
+
+		rescue Conekta::ProcessingError => e
+		  puts e.message_to_purchaser 
+		  #la tarjeta no pudo ser procesada
+
+		rescue Conekta::Error => e
+		  puts e.message_to_purchaser 
+		  #un error ocurrió que no sucede en el flujo normal de cobros como por ejemplo un auth_key incorrecto
+
+		end
+	end
+
+	def subscription
+		begin
+		  @customer = Conekta::Customer.create({
+		    name: "Jose Jose",
+		    email: "lews.therin@gmail.com",
+		    phone: "55-5555-5555",
+		    #cards: [params[:conektaTokenId]]  #["tok_a4Ff0dD2xYZZq82d9"]
+		    cards: ["tok_test_visa_1881"]
+		  })
+		rescue Conekta::ParameterValidationError => e
+		  puts e.message_to_purchaser 
+		  #alguno de los parámetros fueron inválidos
+		end
+
+		@plan = Conekta::Plan.create({
+		  id: "new-monthly-internet-plan",
+		  name: "Monthly Internet Bill",
+		  amount: 51000,
+		  currency: "MXN",
+		  interval: "month"
+		})
+
+		@subscription = @customer.create_subscription({
+		  "plan_id": @plan.id
+		})
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  get  '/credit_form',          to: 'static_pages#credit_form'
-  post '/credit',          to: 'static_pages#credit'
-  get  '/spei_form',          to: 'static_pages#spei_form'
-  post '/spei',          to: 'static_pages#spei'
-  get  '/cash_form',          to: 'static_pages#cash_form'
-  post '/cash',          to: 'static_pages#cash'
-  get  '/banorte_form',          to: 'static_pages#banorte_form'
+  
+  get  '/credit_form',  	to: 'static_pages#credit_form'
+  post '/credit',          	to: 'static_pages#credit'
+  
+  get  '/spei_form',        to: 'static_pages#spei_form'
+  post '/spei',          	to: 'static_pages#spei'
+  
+  get  '/cash_form',        to: 'static_pages#cash_form'
+  post '/cash',          	to: 'static_pages#cash'
+  
+  get  '/banorte_form',     to: 'static_pages#banorte_form'
   post '/banorte',          to: 'static_pages#banorte'
+
+  get  '/ondemand_form',    to: 'static_pages#ondemand_form'
+  post '/ondemand',         to: 'static_pages#ondemand'
+
+  get  '/subscription_form',to: 'static_pages#subscription_form'
+  post '/subscription',     to: 'static_pages#subscription'
+
+  post '/webhook',			to: 'static_pages#webhook'
 end
